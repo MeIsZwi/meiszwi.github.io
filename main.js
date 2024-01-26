6c574973963ce19d2fd7117c40678a9e01af0bfa
MeIsZwi <151751358+MeIsZwi@users.noreply.github.com>
2024-01-25 18:18:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var programCode = function(processingInstance) {
+    with (processingInstance) {
+        size(400, 400); 
+        frameRate(30);
+        
+        // Paste code from Khan Academy here:
+        backro
+    }
+};
+
+// Get the canvas that ProcessingJS will use
+var canvas = document.getElementById("mycanvas"); 
+// Pass the function to ProcessingJS constructor
+var processingInstance = new Processing(canvas, programCode); 
